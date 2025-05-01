20f3d1daa314e1c5ca568b5e30a52d91dfb07a75
Kavinkumar L <Kavinkumar.L@odessainc.com>
2025-05-01 16:04:01 +0530

    Issue 1:2

--- Problems/P001.cs
+++ Problems/P001.cs
@@ -12,12 +12,40 @@ namespace LeetCode.Problems.P001
         {
             public int[] TwoSum(int[] nums, int target)
             {
+                for (int i = 0;i<nums.Length;i++)
+                {
+                    for (int j = i+1; j < nums.Length; j++)
+                    {
+                        if (nums[i] + nums[j] == target)
+                        {
+                            return new int[] {i,j};
+                        }
+                    }
+                }
                 throw new ArgumentException("no data");
             }
 
             public void RunTest()
             {
+                int[] nums = { 2, 7, 11, 15 };
+                int target = 9;
+                var result = TwoSum(nums, target);
+                Console.WriteLine($"Result: [{result[0]}, {result[1]}]");
             }
         }
+         /* Uses the dictionary for the O(n)*/
+
+            //public int[] TwoSum(int[] nums, int target)
+            //{
+            //    Dictionary<int, int> map = new Dictionary<int, int>();
+            //    for (int i = 0; i < nums.Length; i++)
+            //    {
+            //        int complement = target - nums[i];
+
+            //        if (map.ContainsKey(complement))
+            //            return new int[] { map[complement], i };
+            //        if (!map.ContainsKey(nums[i]))
+            //            map[nums[i]] = i;
+            //    }
     }
 }
---
+++ Problems/P002.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.P002
+{
+    internal class P002
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode pointer;
+            public ListNode(int val = 0, ListNode pointer = null)
+            {
+                this.val = val;
+                this.pointer = pointer;
+            }
+        }
+        public class Solution
+        {
+            public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+            {
+                ListNode dummyHead = new();
+                ListNode current = dummyHead;
+                int sum = 0;
+                
+                while(l1 != null || l2 != null || sum != 0)
+                {
+                    if(l1 != null)
+                    {
+                        sum += l1.val;
+                        l1 = l1.pointer;
+                    }
+                    if(l2 != null)
+                    {
+                        sum += l2.val;
+                        l2 = l2.pointer;
+                    }
+                    dummyHead.pointer = new ListNode(sum%10);
+                    sum /= 10;
+                    dummyHead = dummyHead.pointer;
+                }
+                return current.pointer;
+
+            }
+            public void RunTest()
+            {
+                ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+
+                ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4, new ListNode(3))));
+                var result = AddTwoNumbers(l1, l2);
+
+                while (result != null)
+                {
+                    Console.Write(result.val);
+                    if (result.pointer != null)
+                        Console.Write(", ");
+                    result = result.pointer;
+                }
+                Console.WriteLine("]");
+            }
+
+        }
+    }
+}
